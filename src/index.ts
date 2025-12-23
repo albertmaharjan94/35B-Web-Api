c9e2a4437b97d59c4395fa9b5e3d38c6ae908862
albert <albert.maharjan94@gmail.com>
2025-12-23 10:09:11 +0545

    dec 22

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import { connectDatabase } from './database/mongodb';
+import { PORT } from './config';
+
+import dotenv from "dotenv";
+dotenv.config();
+// can use .env variable below this
+console.log(process.env.PORT);
+
 import bookRoutes from './routes/book.route';
 
 const app: Application = express();
-const PORT: number = 3000;
+// const PORT: number = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/books', bookRoutes);
@@ -20,9 +28,15 @@ app.get('/', (req: Request, res: Response) => {
 //     res.status(200).json(books);
 // });
 
-app.listen(
-    PORT, 
-    () => {
-        console.log(`Server: http://localhost:${PORT}`);
-    }
-);
+async function startServer() {
+    await connectDatabase();
+
+    app.listen(
+        PORT, 
+        () => {
+            console.log(`Server: http://localhost:${PORT}`);
+        }
+    );
+}
+
+startServer();
--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -6,9 +6,10 @@ const bookController = new BookController();
 const router: Router = Router();
 
 router.get('/', bookController.getBooks);
-
 // make a router that handles GET request that takes bookid
 // /:bookid and calls bookController.getBookById
+// router.get('/:bookid', bookController.getBookById);
 
+router.post('/', bookController.createBook);
 
 export default router;
