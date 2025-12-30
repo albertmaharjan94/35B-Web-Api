ca3e3ff7e04f3e4b474d03e5c0ed30f805c3877e
albert <albert.maharjan94@gmail.com>
2025-12-30 11:09:08 +0545

    dec 29

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,6 +5,7 @@ let authController = new AuthController();
 const router = Router();
 
 router.post("/register", authController.register)
+router.post("/login", authController.login)
 // add remaning routes like login, logout, etc.
 
 export default router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,9 @@
-import { CreateUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
 import { UserRepository } from "../repositories/user.repository";
 import  bcryptjs from "bcryptjs"
 import { HttpError } from "../errors/http-error";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
 
 let userRepository = new UserRepository();
 
@@ -23,5 +25,29 @@ export class UserService {
         // create user
         const newUser = await userRepository.createUser(data);
         return newUser;
+    }
+
+    async loginUser(data: LoginUserDTO){
+        const user =  await userRepository.getUserByEmail(data.email);
+        if(!user){
+            throw new HttpError(404, "User not found");
+        }
+        // compare password
+        const validPassword = await bcryptjs.compare(data.password, user.password);
+        // plaintext, hashed
+        if(!validPassword){
+            throw new HttpError(401, "Invalid credentials");
+        }
+        // generate jwt
+        const payload = { // user identifier
+            id: user._id,
+            email: user.email,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            role: user.role
+        }
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '30d' }); // 30 days
+        return { token, user }
     }
 }
